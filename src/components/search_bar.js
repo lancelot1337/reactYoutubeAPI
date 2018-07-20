8cd6ab595cd63960dbdc1801022a57d3a8738b65
lancelot1337 <sudhanshusah2001@gmail.com>
2018-07-21 03:23:03 +0530

    Added code to view the entire event on console

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,17 +10,25 @@ const SearchBar = () => {   //functional component
 
 //ES6 class based component
 class SearchBar extends Component{
+    //declare event handeler
+    onInputChange(event) {
+        //the SyntheticEvent object will be reused and all properties will be nullified after the event callback has been invoked.
+        //event.persist() will remove the synthetic event from the pool and allow references to the event to be retained by user code.
+        event.persist();
+        console.log(event);
+    }
+    
+    // //declare event handeler
+    // onInputChange(event) {
+    //     console.log(event.target.value)
+    // }
     //every class component has a function to render
+
     render() {
         return(
             //onChange is emitted when input changes, it is not UD
             <input onChange = {this.onInputChange} />
         );
-    }
-    
-    //declare event handeler
-    onInputChange(event) {
-        console.log(event.target.value)
     }
 }
 
