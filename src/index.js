1cc0c01ecd3721ea51d26297cad149b645cd0047
lancelot1337 <sudhanshusah2001@gmail.com>
2018-07-22 04:23:47 +0530

    Refactored index.js and added constructor fn.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 //import React
-import React from 'react'; //corelib, manage component
+import React, {Component} from 'react'; //corelib, manage component
 import ReactDOM from 'react-dom'; //domlib
 import SearchBar from './components/search_bar'; //relative path is required
 
@@ -9,16 +9,27 @@ import YTSearch from 'youtube-api-search'
 const API_KEY = `AIzaSyB7ceLcVP5EK_kY4T2ixR65uLbpCmITf-M`
 
  //Demo video seach
- YTSearch({key: API_KEY, term: "Linus Tech Tips"}, (data) => {
-    console.log(data);
- });
+
+
 //Creating a new component that returns html
-const App = () => {
-    return ( 
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component{
+    constructor(props) {
+        super(props);
+        this.state = { videos: [] };
+        YTSearch({key: API_KEY, term: "Linus Tech Tips"}, (videos) => {
+            //this.setState({videos: videos});
+            //for ES6
+            this.setState({videos});
+        });
+    }
+
+    render(){
+        return ( 
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
 //instance of component class
