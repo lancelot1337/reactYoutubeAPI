7231a9a09110356b5cef362c1b8342afa9fe9ab8
lancelot1337 <sudhanshusah2001@gmail.com>
2018-07-22 04:15:35 +0530

    Did demo YTSearch

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import React from 'react'; //corelib, manage component
 import ReactDOM from 'react-dom'; //domlib
 import SearchBar from './components/search_bar'; //relative path is required
 
+//for API search
+import YTSearch from 'youtube-api-search'
+
 const API_KEY = `AIzaSyB7ceLcVP5EK_kY4T2ixR65uLbpCmITf-M`
 
+ //Demo video seach
+ YTSearch({key: API_KEY, term: "Linus Tech Tips"}, (data) => {
+    console.log(data);
+ });
 //Creating a new component that returns html
 const App = () => {
     return ( 
