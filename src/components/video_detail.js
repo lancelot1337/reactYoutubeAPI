6fe2427e8f84450e9e6493696105c3e170dfa2c4
lancelot1337 <sudhanshusah2001@gmail.com>
2018-07-22 06:54:08 +0530

    Added functionality to change selected video

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -6,7 +6,6 @@ const VideoDetail = ({video}) => {
         return <div>Loading...</div>
     }
     
-    console.log(video);
     const videoId = video.id.videoId;
     const url = `https://www.youtube.com/embed/${videoId}`;
 
